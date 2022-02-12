a8f1fa08af8ab58538705b9f287524a5dba0cac7
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-12 17:29:22 -0500

    Add function and Big O for algorithm Insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: Head of the list.
+ *
+ * Return: Always 0.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *nxt = NULL;
+	listint_t *tmp = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	else
+	{
+		nxt = (*list)->next;
+		while (nxt != NULL)
+		{
+			tmp = nxt->next;
+			while (nxt->prev != nxt->prev->n > NULL && nxt->n) /* Orden ascendente */
+			{
+				if (nxt->next != NULL)
+				{
+					nxt->next->prev = nxt->prev;
+				}
+				nxt->prev->next = nxt->next;
+				nxt->next = nxt->prev;
+				nxt->prev = nxt->next->prev;
+				nxt->next->prev = nxt;
+				if (nxt->prev != NULL)
+				{
+					nxt->prev->next = nxt;
+				}
+				else
+				{
+					*list = nxt;
+				}
+				print_list(*list);
+			}
+			nxt = tmp;
+		}
+	}
+}
