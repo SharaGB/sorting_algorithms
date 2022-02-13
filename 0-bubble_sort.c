dd1be616d9683f5171916e98c5109241c88ad578
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 14:56:42 -0500

    Fix check efficiency fail

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
-	int swap = 0; /* Variable para indicar intercambios */
+	/* Variable temporal para mantener */
+	/* los valores mientras se intercambia */
+	int swap = 0;
+	unsigned int tmp = 0; /* Variable para indicar intercambios */
 
 	if (array == NULL)
 	{
@@ -18,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1]) /* Comparar elementos, orden ascendente(>) */
 			{
@@ -26,8 +29,13 @@ void bubble_sort(int *array, size_t size)
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
+				tmp = 1; /* Establecerlo a 1 para indicar que se ha hecho intercambios */
 				print_array(array, size);
 			}
+		}
+		if (tmp == 0) /* Si no hay intercambios se sale del bucle */
+		{
+			break;
 		}
 	}
 }
