6e8b2175426565e468a62941a196f7f539840469
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 14:52:20 -0500

    Fix check efficiency fail

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,9 +18,9 @@ void bubble_sort(int *array, size_t size)
 	}
 	if (size >= 2)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - 1; ++i)
 		{
-			for (j = 0; j < size - 1 - i; j++)
+			for (j = 0; j < size - 1 - i; ++j)
 			{
 				if (array[j] > array[j + 1]) /* Comparar elementos, orden ascendente(>) */
 				{
