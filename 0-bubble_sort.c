8d8334682a8f720baf2d07f30532590de451cae3
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 14:58:45 -0500

    Delete variable unnecessary

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
-	/* Variable temporal para mantener */
-	/* los valores mientras se intercambia */
-	int swap = 0;
-	unsigned int tmp = 0; /* Variable para indicar intercambios */
+	int swap = 0; /* Variable para indicar intercambios */
 
 	if (array == NULL)
 	{
@@ -29,13 +26,8 @@ void bubble_sort(int *array, size_t size)
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
-				tmp = 1; /* Establecerlo a 1 para indicar que se ha hecho intercambios */
 				print_array(array, size);
 			}
-		}
-		if (tmp == 0) /* Si no hay intercambios se sale del bucle */
-		{
-			break;
 		}
 	}
 }
