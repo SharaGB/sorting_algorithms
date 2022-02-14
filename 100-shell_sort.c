503f0397a59212f22a9738dd7fe075b6ce765d96
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 20:09:06 -0500

    Fix interval

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,30 +9,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t interval = 0;
 	size_t i = 0;
 	size_t j = 0;
-	int tmp = 0;
+	size_t interval = 1;
+	int tmp;
 
 	if (array == NULL)
-	{
 		return;
-	}
+
 	while (interval < size / 3)
-	{
 		interval = interval * 3 + 1; /* Secuencia de intervalos(Knuth) */
-	}
+
 	while (interval > 0)
 	{
 		/* Realiza una clasificación de inserción de intervalos */
 		/* Sigue añadiendo elementos hasta que todo el array esté ordenado */
-		for (i = interval; i < size; i += 1)
+		for (i = interval; i < size; i++)
 		{
 			/* Añadir ar[i] a los elementos que han sido ordenados y guardalos en tmp*/
 			tmp = array[i];
 			/* Desplzamos los elementos ordenados hasta que encuentre ña unibación */
 			/* correcta de array[i] */
-			for (j = i; j >= interval && array[j - interval] > tmp; j -= interval)
+			for (j = i; j >= interval && array[j - interval] > tmp; j = j - interval)
 			{
 				array[j] = array[j - interval];
 			}
