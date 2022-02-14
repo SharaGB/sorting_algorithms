650e4bd6172e900ffab3af478dd808fcbd1cae08
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 20:19:35 -0500

    Fix interval

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,10 +11,10 @@ void shell_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	size_t j = 0;
-	size_t interval = 0;
+	size_t interval = 1;
 	int tmp;
 
-	if (array == NULL)
+	if (array == NULL || size < 2) /* Size obligatorio para comprobar */
 	{
 		return;
 	}
