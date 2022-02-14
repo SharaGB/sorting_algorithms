cea84a62983f2fc1a317e4abdc4b5e00397465eb
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-13 19:50:21 -0500

    Add more headers

--- sort.h
+++ sort.h
@@ -23,7 +23,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
+/* For  Quick sort algorithm */
 void swap(int *array, int i, int j);
 int partition(int *array, int low, int high, size_t size);
 void quickSort(int *array, int low, int high, size_t size);
