0c0e21f1f48fc416c04a0977e535b5d032a810a3
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-15 14:27:35 -0500

    Function that sorts using the Counting sort algorithm

---
+++ 102-counting_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+/**
+ * counting_sort - Sorts an array of integers in ascending order
+ * using the Counting sort algorithm.
+ * @array: The array to sort.
+ * @size: The size of the array.
+ *
+ * Return: Always 0.
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int j = 0, k_max = 0; /* Mayor número del array */
+	int swap = 0, num = 0;
+	int *count = NULL, *ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return;
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = array[i];
+		if (array[i] > k_max) /* Encuentra el elemento más grande del array */
+			k_max = array[i];
+	}
+	count = malloc(sizeof(int) * (k_max + 1)); /* Hacemos malloc para count: */
+	/* El tamaño de count debe ser (max+1) pero no podemos declararlo como */
+	/* int count(max+1) ya que no soporta la asignación dinámica de memoria */
+	if (count == NULL)
+		return;
+
+	for (j = 0; j <= k_max; j++)
+		count[j] = 0; /* Inicializar array count */
+	for (i = 0; i < size; i++)
+		count[array[i]]++; /* Almacenar count de cada elemento */
+	for (j = 0; j <= k_max; j++)
+	{
+		swap = count[j]; /* Sobreescribir count para mantenr el siguiente */
+		count[j] = num; /* index de un elemento */
+		num += swap;
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[count[ptr[i]]] = ptr[i]; /* Colocar el elemento en el array ordenado */
+		count[ptr[i]]++;
+	}
+	print_array(count, k_max + 1); /* Función para imprimir el array */
+	free(ptr); /* Libero */
+	free(count);
+}
