07e10d790d9985828bd6df749991e2a076c3d163
SharaGB <shara.garcia.betancur@gmail.com>
2022-02-15 14:32:26 -0500

    Add more prototypes

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,6 +26,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 /* For Quick sort algorithm */
 void swap(int *array, int i, int j);
